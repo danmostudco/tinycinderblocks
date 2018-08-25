907bc7aa74acb907a76eec39ca20349f0e8b2ad7
Dan Morrison <morrison.danr@gmail.com>
2018-08-24 22:27:25 -0400

    set up basic react structure from main.js > app.js > index.js

---
+++ client/src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        Here's the main component
+      </div>
+    );
+  }
+}
+
+export default Main;
