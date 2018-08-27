db3b4c98384128136e7ec18cc998d04294fa65df
Dan Morrison <morrison.danr@gmail.com>
2018-08-26 20:24:26 -0400

    set up two stateless functions to make components for pieces of state (cogs & sprockets), map over them

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import './Main.css';
 
-class Main extends Component {
-  render() {
+const Sprocket = (props) => {
     return (
-      <div>
-        Here's the main component
-      </div>
-    );
-  }
+        <div className="widgetItem">
+            ⚙️ {props.name}
+        </div>
+    )
+}
+
+const Cog = (props) => {
+    return (
+        <div className="widgetItem">
+            🔩 {props.name}
+        </div>
+    )
+}
+
+// to test, use $r.props.addSprocket() in the command line
+class Main extends Component {
+    render() {
+        const sprockets = this.props.sprockets.map((sprocket, i) => {
+            return <Sprocket key={i} name={sprocket}  />
+        })
+
+        const cogs = this.props.cogs.map((cog, i) => {
+            return <Cog key={i} name={cog}  />
+        })
+
+        return (
+            <div className="widgetContainer">{sprockets} {cogs}</div>
+        )
+    }
 }
 
 export default Main;
